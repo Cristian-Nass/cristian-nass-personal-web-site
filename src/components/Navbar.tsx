2e3029c433c7f1f46e9869481de3c3dfd1028f7c
Cristian Nass <cristian.tirdad@gmail.com>
2024-01-12 02:07:13 +0100

    add hamburger manu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,74 @@
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useMediaQuery} from 'usehooks-ts';
+
 const Navbar = () => {
+  const matches = useMediaQuery('(min-width: 1024px)');
+  const [menuToggle, setMenuToggle] = useState(false);
+  const items = [
+    {title: 'Home', link: '/', id: '1'},
+    {title: 'About', link: '/about', id: '2'},
+    {title: 'Contact', link: '/contact', id: '3'},
+  ];
+
   return (
-    <div className="py-14  lg:py-20 px-12 lg:px-36">
-      <nav className="text-2xl z-20 relative text-stone-300">
-        <ul className="flex gap-12">
-          <Link to={`/`}>
-            <li>Home</li>
-          </Link>
-          <Link to={`/about`}>
-            <li>About</li>
-          </Link>
-          <Link to={`/contact`}>
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
+    <>
+      {matches ? (
+        <div className="py-14  lg:py-20 px-12 lg:px-36">
+          <nav className="text-2xl z-20 relative text-stone-300 hamburger-manu">
+            <ul className="flex gap-12">
+              {items.map((item) => (
+                <Link key={item.id} to={item.link}>
+                  <li>{item.title}</li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      ) : (
+        <div className="py-6  lg:py-20 px-12 lg:px-36">
+          <nav className="text-2xl z-20 relative text-stone-300 hamburger-manu">
+            <ul className="flex gap-12">
+              <div
+                onClick={() => setMenuToggle(!menuToggle)}
+                style={{cursor: 'pointer', padding: '4px 0px'}}>
+                <div
+                  style={{
+                    width: '26px',
+                    height: '2px',
+                    backgroundColor: 'white',
+                  }}></div>
+                <div
+                  style={{
+                    marginTop: '6px',
+                    width: '26px',
+                    height: '2px',
+                    backgroundColor: 'white',
+                  }}></div>
+                <div
+                  style={{
+                    marginTop: '6px',
+                    width: '26px',
+                    height: '2px',
+                    backgroundColor: 'white',
+                  }}></div>
+              </div>
+            </ul>
+          </nav>
+          {menuToggle && (
+            <div className="hamburger-manu-container">
+              {items.map((item) => (
+                <div className="menu-items-mobile">
+                  <Link key={item.id} to={item.link}>
+                    <div onClick={() => setMenuToggle(false)}>{item.title}</div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
