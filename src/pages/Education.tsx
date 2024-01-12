8b4249b669e697f646e44e37ae9089ceb3f4d5a3
Cristian Nass <cristian.tirdad@gmail.com>
2024-01-13 00:50:04 +0100

    add education

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,9 +1,34 @@
 import transition from '../transition';
+import {useMediaQuery} from 'usehooks-ts';
+import {education} from '../utils/education';
+
+import '../styles/education.css';
 
 const Education = () => {
+  const matches = useMediaQuery('(min-width: 1024px)');
+
   return (
     <div className="bg-teal-600 bg-cover top-0 left-0 absolute h-full w-full">
-      Education Page
+      <div
+        className="flex flex-row"
+        style={{
+          padding: matches ? '20vh 100px' : '10vh 20px',
+          color: 'lightgray',
+          fontFamily: 'Ubuntu',
+          fontSize: '20px',
+        }}>
+        <div>
+          {education.map((item) => (
+            <div key={item.id} style={{paddingTop: '10px'}}>
+              <div className="education-title">{item.title}</div>
+              <div className="education-text">{item.field}</div>
+              <div className="education-under-text">
+                Certified No. {item.certified}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/utils/education.ts
@@ -0,0 +1,37 @@
+export const education = [
+  {
+    id: '1',
+    title: "Bachelor's degree",
+    year: '2011-2015',
+    field: 'Computer Software Technology Engineering Central organization',
+    certified: '139511101971 (IAU Universitet)',
+  },
+  {
+    id: '2',
+    title: 'Associate degree',
+    year: '2000-2003',
+    field: 'Computer Science',
+    certified: '8121270037 (IAU Universitet)',
+  },
+  {
+    id: '3',
+    title: 'Computer Operator',
+    year: '1999',
+    field: 'Secondary Programming',
+    certified: '1-10-103-103/2 (High-school)',
+  },
+  {
+    id: '4',
+    title: 'Technical & Vocational',
+    year: '1999',
+    field: 'Pascal Programming',
+    certified: '1-3-18066 (High-school)',
+  },
+  {
+    id: '5',
+    title: 'Technical & Vocational Skill',
+    year: '1995-1999',
+    field: 'Advanced Computer Driving',
+    certified: '1-3-19061 (High-school)',
+  },
+];
