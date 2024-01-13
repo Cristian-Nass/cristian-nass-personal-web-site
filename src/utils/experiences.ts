c7b5b389862206361f26a08a634db22925a03cb1
Cristian Nass <cristian.tirdad@gmail.com>
2024-01-13 01:15:33 +0100

    add experinse in ts file

--- src/utils/experiences.ts
+++ src/utils/experiences.ts
@@ -24,4 +24,72 @@ export const experiences = [
         creating robust and efficient web applications
     `,
   },
+  {
+    id: '2',
+    company: 'Nina Tryckeri',
+    year: '2018-2019',
+    job: 'Web App Developer',
+    location: 'Uppsala, Sweden',
+    description: `
+      I have designed and developed responsive and dynamic websites for 
+      restaurants. Incorporating an Admin Control Panel.
+      I utilized Angular, HTML, CSS, Firebase, and Bootstrap to create a 
+      user-friendly interface.
+      Additionally, I have experience working with 
+      Adobe XD, Figma, Photoshop, and other tools for UI design.
+    `,
+  },
+  {
+    id: '3',
+    company: 'Kian',
+    year: '2014-2017',
+    job: 'Web App Developer',
+    location: 'Mashad, Iran',
+    description: `
+      I have been part of teams involved in creating and designing
+      numerous web applications for various industries, tutorials, genres, 
+      and more.
+      As a front-end developer, I primarily focused on tasks related to 
+      HTML, CSS, JavaScript, jQuery, and similar technologies.
+      However, I also had opportunities to contribute as a 
+      back-end developer, gaining experience with PHP, Java, and MySQL.
+      As a hobby, I did make a few little applications for Android with Java, 
+      SQLite, and Eclipse as IDE
+    `,
+  },
+  {
+    id: '4',
+    company: 'Tarh & Tasvir',
+    year: '2010-2013',
+    job: 'Graphic designer',
+    location: 'Mashad, Iran',
+    description: `
+      I worked at one of the largest advertising companies in my town,
+      where we handled various fields of design.
+      Our projects included creating brochures, catalogs, billboards, 
+      advertising magazines, and more.
+      Additionally, I gained experience in 3D design using applications such 
+      as 3D-MAX, as well as a bit of industrial photography.
+      We regularly utilized Photoshop, CorelDraw, and other graphic 
+      applications for our design work. 
+      Moreover, in recent months, I have also undertaken the design of a 
+      few static websites.
+    `,
+  },
+  {
+    id: '5',
+    company: 'Asr Jadid',
+    year: '2005-2008',
+    job: 'Graphic designer',
+    location: 'Mashad, Iran',
+    description: `
+      During my time at this company.
+      A significant portion of my responsibilities involved
+      designing tutorial books for kids. 
+      These books encompassed various activities
+      such as puzzles, coloring pages, games, math exercises, and more. 
+      To accomplish this, we utilized a range of applications including
+      Adobe Photoshop, Freehand, CorelDraw, InDesign, and others.
+    `,
+  },
 ];
