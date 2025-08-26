51bf6d29dc6a801e2d0d09e9eb11f9483cb858e0
Cristian Nass <cristian.tirdad@gmail.com>
2025-08-26 19:59:28 +0200

    save

--- cristian-notes/src/components/Navbar.tsx
+++ cristian-notes/src/components/Navbar.tsx
@@ -2,32 +2,35 @@ import {useState} from 'react';
 import SubMenus from './SubMenus';
 const Navbar = () => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
-  const handleSubMenuOpen = () => {
+  const handleSubMenuOpen = (item: string) => {
     setIsSubMenuOpen(true);
+    setSelectedItem(item);
   };
 
-  const handleSubMenuClose = () => {
-    setIsSubMenuOpen(false);
-  };
+  const menuItems = [
+    {
+      label: 'HTML',
+      href: '#html',
+    },
+    {
+      label: 'CSS',
+      href: '#css',
+    },
+    {
+      label: 'JS/TS',
+      href: '#js-ts',
+    },
+    {
+      label: 'React',
+      href: '#react',
+    },
+  ];
 
   return (
     <>
-      <nav
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '40px',
-          backgroundColor: '#333',
-          display: 'flex',
-          alignItems: 'center',
-          paddingLeft: '20px',
-          paddingRight: '20px',
-          zIndex: 1000,
-          boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-        }}>
+      <nav className="main-navbar">
         <ul
           style={{
             display: 'flex',
@@ -38,45 +41,24 @@ const Navbar = () => {
             //   justifyContent: 'center',
             width: '100%',
           }}>
-          <li>
-            <a
-              href="#html"
-              className="navbar-button"
-              onMouseEnter={handleSubMenuOpen}
-              onMouseLeave={handleSubMenuClose}>
-              HTML
-            </a>
-          </li>
-          <li>
-            <a
-              href="#css"
-              className="navbar-button"
-              onMouseEnter={handleSubMenuOpen}
-              onMouseLeave={handleSubMenuClose}>
-              CSS
-            </a>
-          </li>
-          <li>
-            <a
-              href="#js-ts"
-              className="navbar-button"
-              onMouseEnter={handleSubMenuOpen}
-              onMouseLeave={handleSubMenuClose}>
-              JS/TS
-            </a>
-          </li>
-          <li>
-            <a
-              href="#react"
-              className="navbar-button"
-              onMouseEnter={handleSubMenuOpen}
-              onMouseLeave={handleSubMenuClose}>
-              React
-            </a>
-          </li>
+          {menuItems.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="navbar-button"
+                onClick={() => handleSubMenuOpen(item.label)}>
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
-      {isSubMenuOpen && <SubMenus />}
+      {isSubMenuOpen && (
+        <SubMenus
+          selectedItem={selectedItem}
+          setIsSubMenuOpen={setIsSubMenuOpen}
+        />
+      )}
     </>
   );
 };
--- cristian-notes/src/components/SubMenus.tsx
+++ cristian-notes/src/components/SubMenus.tsx
@@ -1,5 +1,23 @@
-const SubMenus = () => {
-  return <div className="sub-menu">SubMenus</div>;
+const SubMenus = ({
+  selectedItem,
+  setIsSubMenuOpen,
+}: {
+  selectedItem: string | null;
+  setIsSubMenuOpen: (isOpen: boolean) => void;
+}) => {
+  const handleSubMenuToggle = (item: string | null) => {
+    console.log(item);
+    setIsSubMenuOpen(false);
+  };
+  return (
+    <div className="sub-menu">
+      <div
+        className="sub-menu-item"
+        onClick={() => handleSubMenuToggle(selectedItem)}>
+        {selectedItem}
+      </div>
+    </div>
+  );
 };
 
 export default SubMenus;
