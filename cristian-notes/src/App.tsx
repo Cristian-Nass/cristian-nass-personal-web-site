fed1b46c4b4ff3e04ce46aec0166538f7caf7857
Cristian Nass <cristian.tirdad@gmail.com>
2025-11-23 17:31:00 +0100

    SAVE

--- cristian-notes/src/App.tsx
+++ cristian-notes/src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 import BlogPage from './pages/BlogPage';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import useDataBlogStore from './store/useDataBlogStore';
 function App() {
+  const { setDataBlog } = useDataBlogStore();
+  useEffect(() => {
+    setDataBlog();
+  }, []);
   return (
     <div>
       <Routes>
